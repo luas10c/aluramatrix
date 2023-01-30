6ff054c758691e4e112a4aa773f5a0aa0ff186a5
Luciano Alves <luas10c@gmail.com>
2023-01-30 19:34:36 -0300

    feat: commit bomb

--- src/components/Sticker.tsx
+++ src/components/Sticker.tsx
@@ -1,14 +1,18 @@
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import axios from "axios";
 
-export default function Sticker({ sticker_url, width = "100%", height = "100%" }) {
-  const [url, setUrl] = useState('');
+export default function Sticker({
+  sticker_url,
+  width = "100%",
+  height = "100%",
+}) {
+  const [url, setUrl] = useState("");
 
   useEffect(() => {
     async function fecthSticker() {
       try {
-        const { data } = await axios.get(sticker_url, { responseType: 'blob' });
+        const { data } = await axios.get(sticker_url, { responseType: "blob" });
         setUrl(URL.createObjectURL(data));
       } catch {
         ///
@@ -20,9 +24,13 @@ export default function Sticker({ sticker_url, width = "100%", height = "100%" }
     }
   }, [sticker_url]);
 
-  return url ? (
+  if (!url) {
+    return null;
+  }
+
+  return (
     <div className="container">
-      <Image src={url} width={width} height={height} alt="#" />
+      <Image src={url} alt="#" />
       <style jsx>{`
         .container {
           display: flex;
@@ -30,5 +38,5 @@ export default function Sticker({ sticker_url, width = "100%", height = "100%" }
         }
       `}</style>
     </div>
-  ) : null
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,9 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return initialProps
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
   }
 
   render() {
@@ -11,16 +11,23 @@ class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@300;400;700&display=swap" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@300;400;700&display=swap"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
